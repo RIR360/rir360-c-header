1919daf9db1ee19caa3fe285a88a4d783c0ff5ab
Rezowan Islam <50569315+RIR360@users.noreply.github.com>
2020-02-11 21:05:00 +0600

    creating the header file

---
+++ rir360.h
@@ -0,0 +1,145 @@
+/*
+* rir360.h
+* Custom header file
+* Created by RIR(Rizvy)
+*/
+
+// *** standard libraries ***
+
+
+// *** macros ***
+#define PI 3.1415926536
+#define SQUARE(x) (x*x)
+#define CUBE(x) (x*x*x)
+
+// *** library functions ***
+// exponent: base to the power of n
+float expo(float base, float power)
+{
+    float expon = 1;
+    for (int i = 0; i < power; i++) {
+        expon *= base;
+    }
+    return expon;
+}
+// converts degree to radian
+float degToRad(float deg)
+{
+    return (deg * PI) / 180;
+}
+// converts radian to degree
+float radToDeg(float rad)
+{
+    return (rad * 180) / PI;
+}
+// returns factorial of an integer
+int fact(int num)
+{
+    int factorial = 1;
+    for (int i = 0; i < num; i++) {
+        factorial *= num - i;
+    }
+    return factorial;
+}
+// nCr combination
+int nCr(int n, int r)
+{
+    return fact(n)/(fact(n-r)*fact(r));
+}
+// converts celcius to fahrenheit
+float celToFah(float cel)
+{
+    return ((cel*9)/5) + 32;
+}
+// converts fahrenheit to celcius
+float fahToCel(float fah)
+{
+    return ((fah - 32)*5) /9;
+}
+// returns the area of circle from radius
+float areaOfCir(float r)
+{
+    return PI * SQUARE(r);
+}
+// repeats a function for n times
+void repeat(void (*f)(), int n)
+{
+    for(int i = 0; i < n; i++) {
+        f();
+    }
+}
+// return largest number from two
+float LargeF2(float a, float b)
+{
+    return (a>b) ? a : b;
+}
+// return smallest number from two
+float smallF2(float a, float b)
+{
+    return (a>b) ? b : a;
+}
+// return largest number from three
+float largeF3(float a, float b, float c)
+{
+    float output;
+    if(a>b && a>c)
+        output = a;
+    if(b>a && b>c)
+        output = b;
+    if(c>b && c>a)
+        output = c;
+
+    return output;
+}
+// return smallest number from three
+float smallF3(float a, float b, float c)
+{
+    float output;
+    if(a<b && a<c)
+        output = a;
+    if(b<a && b<c)
+        output = b;
+    if(c<b && c<a)
+        output = c;
+
+    return output;
+}
+// takes integer input from user and returns it
+// sends error message if the input wasn't int
+int getInt(char prompt[])
+{
+    int out;
+    printf("%s", prompt);
+
+    if(!scanf("%d", &out))
+        printf("Error: You were supposed put an integer.\n\a");
+        out = 0;
+
+    return out;
+}
+// takes float input from user and returns it
+// sends error message if the input wasn't float
+float getFloat(char prompt[])
+{
+    float out;
+    printf("%s", prompt);
+
+    if(!scanf("%f", &out))
+        printf("Error: You were supposed put a float.\n\a");
+        out = 0.0;
+
+    return out;
+}
+// takes string input from user and returns it
+/*
+char* getString(char prompt[])
+{
+    char* out[500];
+    printf("%s", prompt);
+    //scanf("%s", &out);
+
+    out[500] = "rizvy";
+
+    return out;
+}
+*/
