d4724e27d616d25ca8b8c3cea36c22b96af800a8
Rezowan Islam <50569315+RIR360@users.noreply.github.com>
2020-02-20 00:42:11 +0600

    Update rir360.h

--- rir360.h
+++ rir360.h
@@ -14,7 +14,7 @@
 
 // *** custom types ***
 typedef char sstr[50]; // short string holds 50 characters
-typedef char str[100]; // string holds 100 characters
+typedef char str[100]; // string holds unknown characters
 typedef char lstr[500]; // long string holds 500 characters
 typedef char llstr[1000]; // long long string holds 1000 characters
 
@@ -74,42 +74,6 @@ void repeat(void (*f)(), int n)
         f();
     }
 }
-// return largest number from two
-float LargeF2(float a, float b)
-{
-    return (a>b) ? a : b;
-}
-// return smallest number from two
-float smallF2(float a, float b)
-{
-    return (a>b) ? b : a;
-}
-// return largest number from three
-float largeF3(float a, float b, float c)
-{
-    float output;
-    if(a>b && a>c)
-        output = a;
-    if(b>a && b>c)
-        output = b;
-    if(c>b && c>a)
-        output = c;
-
-    return output;
-}
-// return smallest number from three
-float smallF3(float a, float b, float c)
-{
-    float output;
-    if(a<b && a<c)
-        output = a;
-    if(b<a && b<c)
-        output = b;
-    if(c<b && c<a)
-        output = c;
-
-    return output;
-}
 // takes integer input from user and returns it
 // sends error message if the input wasn't int
 int getInt(char prompt[])
@@ -149,3 +113,89 @@ char* getString(char prompt[])
     return out;
 }
 */
+// transform string case to lower case
+void toLower(char text[])
+{
+    int count = strlen(text);
+
+    for(int i = 0; i < count; i++) {
+        if(text[i] > 64 && text[i] < 97) {
+            text[i] += 32;
+        } else text[i];
+    }
+}
+// transform string case to upper case
+void toUpper(char text[])
+{
+    int count = strlen(text);
+
+    for(int i = 0; i < count; i++) {
+        if(text[i] > 96) {
+            text[i] -= 32;
+        } else text[i];
+    }
+}
+// linear search
+// returns the index/position of n(th) match for specific integer target in an array
+int lSearch(int target, int matchNo, int array[], int arrayLength)
+{
+    int match = 0;
+    int matches = 0;
+
+    for(int i = 0; i < arrayLength; i++) {
+        if(array[i] == target) {
+            matches++;
+            if(matches == matchNo) {
+                match = i;
+                break;
+            }
+        }
+    }
+    return match;
+}
+// linear search matches
+// returns the available matches for specific integer target in an array
+int lMatches(int target, int array[], int arrayLength)
+{
+    int matches = 0;
+    for(int i = 0; i < arrayLength; i++) {
+        if(array[i] == target) {
+            matches++;
+        }
+    }
+    return matches;
+}
+// ask box
+// prompts a question on stdout in a box scans for input and sends 1(if input was y) if or 0(if input was n) to the check variable.
+void askBox(char text[],int* check)
+{
+    int textLength = strlen(text), length = 9;
+    char out[50];
+
+    if(textLength > length) {
+        length = textLength;
+    }
+
+    printf("\n");
+    for (int i = 0; i < length + 3; i++) {
+        printf("=");
+    }
+    printf("\n\n %s\n", text);
+    printf("\n [y]yes   [n]no\n\n");
+    for (int i = 0; i < length + 3; i++) {
+        printf("=");
+    }
+    printf("::==> ");
+    scanf(" %s", &out);
+
+    if(out[0] == 'y') {
+        printf("Yes registered.");
+        *check = 1;
+    } else if(out[0] == 'n') {
+        printf("No registered.");
+        *check = 0;
+    } else {
+        printf("You're supposed put yes or no.\a");
+        check = 2;
+    }
+}
